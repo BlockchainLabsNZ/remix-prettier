65e339e9d3241c10f6eabebb9a76454cef9b451b
Klaus Hott Vidal <klahott@gmail.com>
2019-07-04 11:52:50 +1200

    build

---
+++ build/precache-manifest.66115b605d44039893fc085054b0ad8a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "221041f89b76bedbce6d4a865fbd9ebb",
+    "url": "/BlockchainLabsNZ/remix-prettier/index.html"
+  },
+  {
+    "revision": "ce0f40b022af6d0ec08a",
+    "url": "/BlockchainLabsNZ/remix-prettier/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "7518c0c861103da428a8",
+    "url": "/BlockchainLabsNZ/remix-prettier/static/css/main.0b6788b2.chunk.css"
+  },
+  {
+    "revision": "ce0f40b022af6d0ec08a",
+    "url": "/BlockchainLabsNZ/remix-prettier/static/js/2.cf318ce7.chunk.js"
+  },
+  {
+    "revision": "7518c0c861103da428a8",
+    "url": "/BlockchainLabsNZ/remix-prettier/static/js/main.61c9cc19.chunk.js"
+  },
+  {
+    "revision": "6bd6ce891adc241de6db",
+    "url": "/BlockchainLabsNZ/remix-prettier/static/js/runtime~main.0e28ad00.js"
+  },
+  {
+    "revision": "57c8d7653b2657e77ae239a93772f1eb",
+    "url": "/BlockchainLabsNZ/remix-prettier/static/media/prettier.57c8d765.ttf"
+  },
+  {
+    "revision": "7fcdafe541564eb0ea682b49d18691fe",
+    "url": "/BlockchainLabsNZ/remix-prettier/static/media/prettier.7fcdafe5.eot"
+  },
+  {
+    "revision": "8400e4b188041a59d8be015fd8d12f6f",
+    "url": "/BlockchainLabsNZ/remix-prettier/static/media/prettier.8400e4b1.woff"
+  },
+  {
+    "revision": "842efcd08d0c314cca9ce6666b630664",
+    "url": "/BlockchainLabsNZ/remix-prettier/static/media/prettier.842efcd0.svg"
+  }
+]);
