24b874c952be33e52b6971488054b06ee3d42f74
Patrick Gallagher <blockchainbuddha@gmail.com>
2019-09-19 13:25:27 -0400

    Add (rough draft) Profile file
    Hey thanks for working on this plugin!
    I started making the profile which will be added to the [remixAppManager](https://github.com/ethereum/remix-ide/blob/master/src/remixAppManager.js) when you're ready to launch.
    It's helpful to include the "documentation" link, which is why I included it here, since not everyone is aware of this.
    Rock on :guitar: :musical_note: :fire:

---
+++ profile.js
@@ -0,0 +1,12 @@
+  const remix-prettier = {
+      name: 'remix-prettier',
+      displayName: 'Remix Prettier',
+      events: [],
+      methods: [],
+      version: '0.1.0',
+      url: 'https://remix-prettier.netlify.com/',
+      description: 'Format contracts using Prettier',
+      documentation: 'https://github.com/BlockchainLabsNZ/remix-prettier',
+      icon: 'https://remix-prettier.netlify.com/logo.svg',
+      location: 'sidePanel'
+    }
